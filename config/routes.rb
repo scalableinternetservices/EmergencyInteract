42bcb1a68452da1a526c09a1c7359e48f09fac51
hwright-ucsb <wright@umail.ucsb.edu>
2016-10-11 20:49:23 -0700

    Basic static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/home',	to: 'static_pages#home'
 
-  get 'static_pages/help'
+  get '/help',	to: 'static_pages#help'
 
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
