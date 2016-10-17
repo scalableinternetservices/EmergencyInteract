d44a7b339566f15e8363bf27043fa05a72d0be43
Alan Buzdar <thebuzdar@gmail.com>
2016-10-16 23:14:43 -0700

    automatically get address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009234624) do
+ActiveRecord::Schema.define(version: 20161017034011) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161009234624) do
     t.integer  "interactions"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "lat"
+    t.float    "long"
   end
 
 end
