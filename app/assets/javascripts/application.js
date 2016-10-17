9ceb70729f8ef2e944386704a6b30b80a0ca5bc2
hwright-ucsb <wright@umail.ucsb.edu>
2016-10-17 13:05:50 -0700

    add markers from event locations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//
+// app/assets/javascripts/application.js
+//= require_tree ./channels
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get '/help',	to: 'static_pages#help'
 
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+  
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
