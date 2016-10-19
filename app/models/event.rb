5975f71667f7b012f641c224da3c8af324f59a26
Suraj-Rajesh <surajx.rajesh@gmail.com>
2016-10-18 23:34:17 -0700

    Search feature

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+
+  def self.search(search)
+    if search
+      self.where("title LIKE :search OR description LIKE :search OR location LIKE :search", search: "%#{search}%")
+    else
+      self.all
+    end
+  end
 end
