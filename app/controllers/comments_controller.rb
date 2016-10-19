ee115352b631b597736577c0878bb919f7fd1014
Suraj-Rajesh <surajx.rajesh@gmail.com>
2016-10-19 16:53:30 -0700

    Comments body shown

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
     def create
       @event = Event.find(params[:event_id])
-      @comment = @event.comments.create#(comment_params)
+      @comment = @event.comments.create(comment_params)
       redirect_to event_path(@event)
     end
      
     private
       def comment_params
-               #     params.require(:comment).permit(:commenter, :body)
+        params.require(:comment).permit(:body)
       end
 end
