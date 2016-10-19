9a9de2c3bbb189ea6092467702e872ce2da08c32
Suraj-Rajesh <surajx.rajesh@gmail.com>
2016-10-19 16:49:11 -0700

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get '/help',	to: 'static_pages#help'
 
-  resources :events
+  resources :events do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
 end
