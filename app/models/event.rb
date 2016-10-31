6fc51a42032de5d9d345e299ed5a18afdf922537
Alan Buzdar <thebuzdar@gmail.com>
2016-10-30 22:36:14 -0700

    image stuff

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   has_many :comments 
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   def self.search(search)
     if search
       self.where("title LIKE :search OR description LIKE :search OR location LIKE :search", search: "%#{search}%")
--- app/views/events/_event.json.jbuilder
+++ app/views/events/_event.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! event, :id, :title, :description, :location, :followers, :interactions, :created_at, :updated_at
+json.extract! event, :id, :title, :description, :location, :followers, :interactions, :created_at, :updated_at, :image
 json.url event_url(event, format: :json)
