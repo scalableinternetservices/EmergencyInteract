82e6219f59e8c7a10732551d48dfa7b6979a9ad9
Yantsey Tsai <yantseytsai@gmail.com>
2016-11-03 13:41:26 -0700

    add javascript with cities - hardcoded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+
+100.times do |index|
+  Event.create!(title: "Title #{index}",
+                description: "Description #{index}",
+                location: "location #{index}",
+                interactions: 5)
+end
+
+p "Created #{Event.count} events"
